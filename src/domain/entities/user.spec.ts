962afe6508f9ff1cb994285496c5ab47628f58c6
Matteus Santos S <matteus.py@gmail.com>
2024-08-12 20:07:00 -0300

    test: add test to password validation to user entity

--- src/domain/entities/user.spec.ts
+++ src/domain/entities/user.spec.ts
@@ -6,9 +6,37 @@ describe('User entity', () => {
   it('should be able create an instance of User', () => {
     const user = new User({
       email: new Email('fake@email.com'),
-      password: 'fake-password'
+      password: 'F@k3Password'
     })
 
     expect(user).instanceOf(User)
+  })
+  it('should throw an error for a password without an number', () => {
+    expect(
+      () => new User({
+        email: new Email('fake@email.com'),
+        password: 'f@kePassword'
+      })).toThrowError()
+  })
+  it('should throw an error for a password without a capital letter', () => {
+    expect(
+      () => new User({
+        email: new Email('fake@email.com'),
+        password: 'f@k#password'
+      })).toThrowError()
+  })
+  it('should throw an error for a password without a lower-case letter', () => {
+    expect(
+      () => new User({
+        email: new Email('fake@email.com'),
+        password: 'F@K3PASSWORD'
+      })).toThrowError()
+  })
+  it('should throw an error for passwords without special characters', () => {
+    expect(
+      () => new User({
+        email: new Email('fake@email.com'),
+        password: 'fak3Password'
+      })).toThrowError()
   })
 })
