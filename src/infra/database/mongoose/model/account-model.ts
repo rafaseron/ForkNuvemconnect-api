fe1ed0169940550c894545b35faa3e4d0882b8a5
nandajfa <nanda.jfa@gmail.com>
2024-08-26 11:42:27 -0300

    feat: add token to accountModel

--- src/infra/database/mongoose/model/account-model.ts
+++ src/infra/database/mongoose/model/account-model.ts
@@ -2,9 +2,12 @@ import { model, Schema } from 'mongoose'
 import { AccountProps } from '../../../../domain/entities/account'
 import { Replace } from '../../../../domain/utils/replace'
 
-
-export const accountModel = model<Replace<AccountProps, {email: string}>>('Account', new Schema({
-  uuid: String,
-  email: String,
-  password: String
-}))
+export const accountModel = model<Replace<AccountProps, { email: string }>>(
+  'Account',
+  new Schema({
+    uuid: String,
+    email: String,
+    password: String,
+    token: String
+  })
+)
