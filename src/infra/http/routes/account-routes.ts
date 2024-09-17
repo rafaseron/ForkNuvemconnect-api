4ecad70b444e45fd5f54bd561e57abc78a2b98c8
nandajfa <nanda.jfa@gmail.com>
2024-09-17 20:45:37 -0300

    feat: create reset and forgot password route

--- src/infra/http/routes/account-routes.ts
+++ src/infra/http/routes/account-routes.ts
@@ -5,6 +5,7 @@ import { CreateAccountUseCase } from '../../../use-cases/user/create-account-use
 import { AccountRepositoryMongoose } from '../../database/mongoose/repositories/account-repository-mongoose'
 import { BadRequestError } from '../../../domain/utils/error-handle'
 import { LoginUseCase } from '../../../use-cases/user/login-use-case'
+import { resetPasswordUseCase } from '../../../use-cases/user/reset-password-use-case'
 
 export async function accountRoute (fastify: FastifyInstance) {
   fastify.withTypeProvider<ZodTypeProvider>().post(
@@ -53,6 +54,55 @@ export async function accountRoute (fastify: FastifyInstance) {
       const loginUseCase = new LoginUseCase(accountRepository)
       const token = await loginUseCase.execute({ email, password })
       res.send({ token })
+    }
+  )
+
+  // fastify.withTypeProvider<ZodTypeProvider>().post(
+  //   '/forgot-password',
+  //   {
+  //     schema: {
+  //       body: z.object({
+  //         email: z.string().email()
+  //       })
+  //     }
+  //   },
+  //   async (req, res) => {
+  //     const { email } = req.body
+
+  //     const accountRepository = new AccountRepositoryMongoose()
+  //     const account = await accountRepository.findByEmail(email)
+  //     if (!account) {
+  //       throw new BadRequestError('E-mail not found')
+  //     }
+  //     sendEmail = new sendEmailUseCase(accountRepository)
+  //     const verificationCode = await sendEmail.execute(email)
+
+  //     res.send({ verificationCode })
+  //   }
+  // )
+
+  fastify.withTypeProvider<ZodTypeProvider>().put(
+    '/reset-password',
+    {
+      schema: {
+        body: z.object({
+          email: z.string().email(),
+          password: z.string(),
+          passwordConfirmation: z.string()
+        })
+      }
+    },
+    async (req, res) => {
+      const { email, password, passwordConfirmation } = req.body
+      if (password != passwordConfirmation) {
+        throw new BadRequestError(
+          'password confirmation different from password'
+        )
+      }
+      const accountRepository = new AccountRepositoryMongoose()
+      const resetPassword = new resetPasswordUseCase(accountRepository)
+      const result = await resetPassword.execute({ email, password })
+      return res.send({ message: result })
     }
   )
 }
