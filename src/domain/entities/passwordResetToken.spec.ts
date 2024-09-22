4593ca608e8b57221d3fdca7e79f4d95ec425859
Matteus Santos S <matteus.py@gmail.com>
2024-09-21 21:20:25 -0300

    test(PasswordResetTokenSpec): add test case

--- src/domain/entities/passwordResetToken.spec.ts
+++ src/domain/entities/passwordResetToken.spec.ts
@@ -7,13 +7,17 @@ describe('PasswordResetToken entity', () => {
 
     expect(passwordResetToken).instanceOf(PasswordResetToken)
   })
-  it('should throw error if token then 6 length and non alfa-numeric', () => {
+  it('should throw an error if the token is not exactly 6 characters long and is not alphanumeric', () => {
     expect(() => {
       PasswordResetToken.create('john@email.com', '1e3f5')
     }).toThrowError()
 
     expect(() => {
       PasswordResetToken.create('john@email.com', '123456')
+    }).toThrowError()
+
+    expect(() => {
+      PasswordResetToken.create('john@email.com', '12f45s7')
     }).toThrowError()
   })
 })
