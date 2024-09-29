d18a0211074ca222801d0da00514e70abf20bb0b
Matteus Santos S <matteus.py@gmail.com>
2024-09-29 19:04:03 -0300

    feat(AccountRepository): add update method

--- src/domain/repositories/account-repository.ts
+++ src/domain/repositories/account-repository.ts
@@ -1,9 +1,12 @@
 import { Email } from '../entities/email'
-import { Account } from '../entities/account'
+import { Account, AccountProps } from '../entities/account'
+
+export type updateAccountType = Partial<Omit<AccountProps, 'uuid'>>
 
 export interface IAccountRepository {
   save(account: Account): Promise<void>
   findByEmail(email: string): Promise<Account | null>
   findByEmailPassword(email: Email, password: string): Promise<string | null>
   updatePassword(email: string, password: string): Promise<void>
+  update(uuid: string, accountProps: updateAccountType): Promise<void>
 }
