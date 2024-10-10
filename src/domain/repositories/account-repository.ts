9b4457f08d30526039c0cd7454228d1ebf22e603
Matteus Santos S <matteus.py@gmail.com>
2024-10-10 10:20:44 -0300

    feat: change the return of findByEmailPassowrd

--- src/domain/repositories/account-repository.ts
+++ src/domain/repositories/account-repository.ts
@@ -1,12 +1,12 @@
-import { Email } from '../entities/email'
-import { Account, AccountProps } from '../entities/account'
+import type { Email } from '../entities/email'
+import type { Account, AccountProps } from '../entities/account'
 
 export type updateAccountType = Partial<Omit<AccountProps, 'uuid'>>
 
 export interface IAccountRepository {
   save(account: Account): Promise<void>
   findByEmail(email: string): Promise<Account | null>
-  findByEmailPassword(email: Email, password: string): Promise<string | null>
+  findByEmailPassword(email: Email, password: string): Promise<Account | null>
   updatePassword(email: string, password: string): Promise<void>
   update(uuid: string, accountProps: updateAccountType): Promise<void>
 }
